35e858c840bc7c39136feb3b2463e46f6350aca0
8c6794b6 <8c6794b6@gmail.com>
2020-01-30 11:55:00 +0900

    Update URL parameters
    Add "QueryParam" type class, add "Travis", "CircleCI", and "Jenkins"
    data type, define instances for "QueryParam".
    
    Modify "getUrlApiV2" function, use "QueryParam" and data types for
    continuous integration.

--- exec/CodecovHaskellMain.hs
+++ exec/CodecovHaskellMain.hs
@@ -20,28 +20,100 @@ import           CodecovHaskellCmdLine
 baseUrlApiV2 :: String
 baseUrlApiV2 = "https://codecov.io/upload/v2"
 
-getUrlApiV2 :: IO String
-getUrlApiV2 = do
-    env <- getEnvironment
-    case snd <$> find (isJust . flip lookup env . fst) ciEnvVars of
-        Just (service, idParamEnvVar, commitEnvVar, branchEnvVar) -> do
-            idParamValue <- getEnv idParamEnvVar
-            commit <- getEnv commitEnvVar
-            branch <- getEnv branchEnvVar
-            return $ baseUrlApiV2 ++
-                     "?job=" ++ idParamValue ++
-                     "&commit=" ++ commit ++
-                     "&branch=" ++ branch ++
-                     "&service=" ++ service
-        _ -> error "Unsupported CI service."
-    where ciEnvVars = [
-           ("TRAVIS", ("travis", "TRAVIS_JOB_ID", "TRAVIS_COMMIT", "TRAVIS_BRANCH")),
-           ("JENKINS_HOME", ("jenkins", "BUILD_NUMBER", "GIT_COMMIT", "GIT_BRANCH")),
-           ("CIRCLECI", ("circleci", "CIRCLE_BUILD_NUM", "CIRCLE_SHA1", "CIRCLE_BRANCH"))]
+class QueryParam q where
+  qp_service :: q -> String
+  qp_service = const "unknown"
+  qp_branch  :: q -> IO String
+  qp_branch  = const (return "")
+  qp_build   :: q -> IO String
+  qp_build   = const (return "")
+  qp_commit  :: q -> IO String
+  qp_commit  = const (return "")
+  qp_job     :: q -> IO String
+  qp_job     = const (return "")
+  qp_slug    :: q -> IO String
+  qp_slug    = const (return "")
+  qp_env     :: q -> IO String
+  qp_env     = const (return "")
+  qp_tag     :: q -> IO String
+  qp_tag     = const (return "")
+  qp_pr      :: q -> IO String
+  qp_pr      = const (return "")
 
-getUrlWithToken :: String -> String -> Maybe String -> IO String
-getUrlWithToken apiUrl _ Nothing = return apiUrl
-getUrlWithToken apiUrl param (Just t) = return $ apiUrl ++ "&" ++ param ++ "=" ++ t
+data Travis = Travis
+
+instance QueryParam Travis where
+  qp_service _ = "travis"
+  qp_branch _  = getEnv "TRAVIS_BRANCH"
+  qp_build _   = getEnv "TRAVIS_JOB_NUMBER"
+  qp_commit _  = getEnv "TRAVIS_COMMIT"
+  qp_job _     = getEnv "TRAVIS_JOB_ID"
+  qp_slug _    = getEnv "TRAVIS_REPO_SLUG"
+  qp_env _     = getEnv "TRAVIS_OS_NAME"
+  qp_tag _     = getEnv "TRAVIS_TAG"
+  qp_pr _      = getEnv "TRAVIS_PULL_REQUEST"
+
+data CircleCI = CircleCI
+
+instance QueryParam CircleCI
+
+data Jenkins = Jenkins
+
+instance QueryParam Jenkins
+
+composeParam :: QueryParam ci => ci -> IO String
+composeParam ci =
+  do let service = qp_service ci
+         get_val acc (g,key) = do
+           val <- g ci
+           return ((key ++ '=':val) : acc)
+         kvs = [(qp_branch, "branch")
+               ,(qp_build, "build")
+               ,(qp_commit, "commit")
+               ,(qp_job, "job")
+               ,(qp_slug, "slug")
+               ,(qp_env, "env")
+               ,(qp_tag, "tag")
+               ,(qp_pr, "pr")]
+     params <- foldM get_val [("service" ++ '=':service)] kvs
+     return $ concat (intersperse "&" params)
+
+getUrlApiV2 :: IO String
+getUrlApiV2 =
+  do env <- getEnvironment
+     let make_params
+           | has "TRAVIS"       = composeParam Travis
+           | has "CIRCLECI"     = composeParam CircleCI
+           | has "JENKINS_HOME" = composeParam Jenkins
+           | otherwise          = error "Unsupported CI service."
+           where has key = isJust (lookup key env)
+     params <- make_params
+     return $ baseUrlApiV2 ++ '?':params
+
+-- getUrlApiV2 :: IO String
+-- getUrlApiV2 = do
+--     env <- getEnvironment
+--     case snd <$> find (isJust . flip lookup env . fst) ciEnvVars of
+--         Just (service, idParamEnvVar, commitEnvVar, branchEnvVar) -> do
+--             idParamValue <- getEnv idParamEnvVar
+--             commit <- getEnv commitEnvVar
+--             branch <- getEnv branchEnvVar
+--             return $ baseUrlApiV2 ++
+--                      "?job=" ++ idParamValue ++
+--                      "&commit=" ++ commit ++
+--                      "&branch=" ++ branch ++
+--                      "&service=" ++ service
+--         _ -> error "Unsupported CI service."
+--     where ciEnvVars = [
+--            ("TRAVIS", ("travis", "TRAVIS_JOB_ID", "TRAVIS_COMMIT", "TRAVIS_BRANCH")),
+--            ("JENKINS_HOME", ("jenkins", "BUILD_NUMBER", "GIT_COMMIT", "GIT_BRANCH")),
+--            ("CIRCLECI", ("circleci", "CIRCLE_BUILD_NUM", "CIRCLE_SHA1", "CIRCLE_BRANCH"))]
+
+getUrlWithToken :: String -> String -> Maybe String -> String
+-- getUrlWithToken apiUrl _ Nothing = return apiUrl
+-- getUrlWithToken apiUrl param (Just t) = return $ apiUrl ++ "&" ++ param ++ "=" ++ t
+getUrlWithToken apiUrl param mb_val =
+  apiUrl ++ '$':param ++ '=':fromMaybe "" mb_val
 
 getConfig :: CodecovHaskellArgs -> Maybe Config
 getConfig cha = do _testSuites <- listToMaybe (testSuites cha)
@@ -62,11 +134,11 @@ main = do
             when (displayReport cha) $ BSL.putStrLn $ encode codecovJson
             unless (dontSend cha) $ do
                 apiUrl <- getUrlApiV2
-                fullUrl <- getUrlWithToken apiUrl "token" (token cha)
+                let fullUrl = getUrlWithToken apiUrl "token" (token cha)
                 response <- postJson (BSL.unpack $ encode codecovJson) fullUrl (printResponse cha)
                 case response of
                     PostSuccess url _ -> do
-                        responseUrl <- getUrlWithToken url "access_token" (accessToken cha)
+                        let responseUrl = getUrlWithToken url "token" (token cha)
                         putStrLn ("URL: " ++ responseUrl)
                         -- wait 10 seconds until the page is available
                         threadDelay (10 * 1000000)
